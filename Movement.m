89d377a261fb661ec4a27b0e433042c56bb56b26
Daniel <daniel.aldazburgoa@student.uts.edu.au>
2020-06-10 13:37:05 +1000

    Update Movement.m

--- Movement.m
+++ Movement.m
@@ -27,7 +27,7 @@ for i=1
     steps = t/deltaT;                   % No. of steps for simulation
     delta = 2*pi/steps;                 % Small angle change
     epsilon = 0.1;                      % Threshold value for manipulability/Damped Least Squares
-    W = diag([1 1 1 0.1 0.1 0.1]);      % Weighting matrix for the velocity vector
+    W = diag([1 1 1 0 0 0]);      % Weighting matrix for the velocity vector (W*xdot)
     board.side1.startCell = 1;              % Side 1 starts at cell 1
     board.side2.startCell = 11;             % Side 2 starts at cell 11
     board.side3.startCell = 21;             % Side 3 starts at cell 21
@@ -41,8 +41,6 @@ for i=1
     qdot = zeros(steps,6);          % Array for joint velocities
     theta = zeros(3,steps);         % Array for roll-pitch-yaw angles
     x = zeros(3,steps);             % Array for x-y-z trajectory
-    positionError = zeros(3,steps); % For plotting trajectory error
-    angleError = zeros(3,steps);    % For plotting trajectory error
 end
 
 %% Animate RMRC
@@ -159,29 +157,37 @@ for j=1
                 Ra = T(1:3,1:3);                                        % Current end-effector rotation matrix
                 Rdot = (1/deltaT)*(Rd - Ra);                            % Calculate rotation matrix error
                 S = Rdot*Ra';                                           % Skew symmetric!
+                
                 linear_velocity = (1/deltaT)*deltaX;
                 angular_velocity = [S(3,2);S(1,3);S(2,1)];              % Check the structure of Skew Symmetric matrix!!
-                deltaTheta = tr2rpy(Rd*Ra');                            % Convert rotation matrix to RPY angles
+                
+                % Velocity vector weighted
                 xdot = W*[linear_velocity;angular_velocity];            % Calculate end-effector velocity to reach next waypoint.
                 J = kinova.jacob0(qMatrix(i,:));                        % Get Jacobian at current joint state
+                                
+                % Singularity check
                 m(i) = sqrt(det(J*J'));
-                if m(i) < epsilon                           % If manipulability is less than given threshold
+                if m(i) < epsilon                                       % If manipulability is less than given threshold
                     lambda = (1 - m(i)/epsilon)*5E-2;
                 else
                     lambda = 0;
                 end
-                invJ = inv(J'*J + lambda *eye(6))*J';                                   % DLS Inverse
-                qdot(i,:) = (invJ*xdot)';                                                % Solve the RMRC equation (you may need to transpose the         vector)
-                for j = 1:6                                                             % Loop through joints 1 to 6
-                    if qMatrix(i,j) + deltaT*qdot(i,j) < kinova.qlim(j,1)                     % If next joint angle is lower than joint limit...
+                invJ = inv(J'*J + lambda *eye(6))*J';                                       % DLS Inverse
+                
+                % Joint velocities
+                qdot(i,:) = (invJ*xdot)';                                                   % Solve the RMRC equation (you may need to transpose the vector)
+                
+                % Check close to joint limits
+                for j = 1:6                                                                 % Loop through joints 1 to 6
+                    if qMatrix(i,j) + deltaT*qdot(i,j) < kinova.qlim(j,1)                   % If next joint angle is lower than joint limit...
                         qdot(i,j) = 0; % Stop the motor
-                    elseif qMatrix(i,j) + deltaT*qdot(i,j) > kinova.qlim(j,2)                 % If next joint angle is greater than joint limit ...
+                    elseif qMatrix(i,j) + deltaT*qdot(i,j) > kinova.qlim(j,2)               % If next joint angle is greater than joint limit ...
                         qdot(i,j) = 0; % Stop the motor
                     end
                 end
+                
+                % Next joint configuration
                 qMatrix(i+1,:) = qMatrix(i,:) + deltaT*qdot(i,:);                         	% Update next joint state based on joint velocities
-                positionError(:,i) = x(:,i+1) - T(1:3,4);                               % For plotting
-                angleError(:,i) = deltaTheta;                                           % For plotting
             end
         end
         
